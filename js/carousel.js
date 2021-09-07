2e224980b3f06d34384b7b70c9ecdd9f73c4ed32
harumihibino <75013705+harumihibino@users.noreply.github.com>
2021-09-07 15:09:10 +0900

    Add files via upload

---
+++ js/carousel.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $('.carousel-img').slick({
+        autoplay: true,
+        autoplaySpeed: 5000,
+        arrows: false,
+        dots: true,
+        fade: true,
+    });
+});
