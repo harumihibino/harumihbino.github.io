349ae9add7d9b26e8785efb39520f4a0ce3005ee
harumihibino <75013705+harumihibino@users.noreply.github.com>
2021-09-07 15:23:22 +0900

    Delete js directory

--- js/carousel.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    $('.carousel-img').slick({
-        autoplay: true,
-        autoplaySpeed: 2500,
-        arrows: false,
-        dots: true,
-        fade: true,
-    });
-});
